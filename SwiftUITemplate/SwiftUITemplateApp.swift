1bd3a4be6444687bc52f27d1e46b1994a1ba1570
Vsevolod <vsevolod19860507@icloud.com>
2023-03-26 12:53:15 +0300

    Initial Commit

---
+++ SwiftUITemplate/SwiftUITemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITemplateApp.swift
+//  SwiftUITemplate
+//
+//  Created by Vsevolod on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

babcaa594db5b230288b8d508a0ff544e0199e22
Vsevolod <vsevolod19860507@icloud.com>
2023-04-09 19:08:04 +0300

    fonts updated

--- SwiftUITemplate/Font.swift
+++ SwiftUITemplate/Font.swift
@@ -4,45 +4,44 @@
 //
 //  Created by Vsevolod on 26.03.2023.
 //
-
 import SwiftUI
 import CustomFont
 
 extension CustomTextStyle {
-    static let hugeTitleFixed = CustomTextStyle(Barlow.semiBold, fixedSize: 36)
-    static let hugeTitle = CustomTextStyle(Barlow.semiBold, size: 36, relativeTo: .largeTitle)
-    static let largeTitle = CustomTextStyle(Copperplate.light, size: 34, relativeTo: .largeTitle)
-    static let largeTitle2 = CustomTextStyle(Barlow.semiBold, size: 32, relativeTo: .largeTitle)
+    /// Barlow, 100, 34
+    static let largeTitle = CustomTextStyle(Barlow.thin, size: 34, relativeTo: .largeTitle)
+    /// NewYorkExtraLarge, 600, 28
     static let title = CustomTextStyle(NewYorkExtraLarge.semiBold, size: 28, relativeTo: .title)
-    static let title2 = CustomTextStyle(Barlow.thin, size: 22, relativeTo: .title2)
-    static let title3 = CustomTextStyle(Barlow.thin, size: 20, relativeTo: .title3)
-    static let headline = CustomTextStyle(Barlow.semiBold, size: 17, relativeTo: .headline)
+    /// NewYorkExtraLarge, 600, 22
+    static let title2 = CustomTextStyle(NewYorkExtraLarge.semiBold, size: 22, relativeTo: .title)
+    /// NewYorkExtraLarge, 600, 20
+    static let title3 = CustomTextStyle(NewYorkExtraLarge.semiBold, size: 20, relativeTo: .title)
+    /// NewYorkExtraLarge, 600, 17
+    static let headline = CustomTextStyle(NewYorkExtraLarge.semiBold, size: 17, relativeTo: .headline)
+    /// Barlow, 400, 17
     static let body = CustomTextStyle(Barlow.regular, size: 17)
-    static let body2 = CustomTextStyle(Barlow.thin, size: 17)
-    static let callout = CustomTextStyle(NewYorkExtraLarge.semiBold, size: 16, relativeTo: .callout)
-    static let subheadline = CustomTextStyle(DancingScript.regular, size: 15, relativeTo: .subheadline)
-    static let footnote = CustomTextStyle(DancingScript.medium, size: 13, relativeTo: .footnote)
-    static let caption = CustomTextStyle(DancingScript.regular, size: 12, relativeTo: .caption)
-    static let caption2 = CustomTextStyle(DancingScript.medium, size: 11, relativeTo: .caption2)
-#if os(macOS)
-    static let caption3 =  CustomTextStyle(NewYorkExtraLarge.semiBold, size: 10, relativeTo: .caption2)
-#else
-    static let caption3 =  CustomTextStyle(NewYorkExtraLarge.semiBold, size: 10, relativeTo: .caption2)
-#endif
+    /// Barlow, 100, 16
+    static let callout = CustomTextStyle(Barlow.thin, size: 16, relativeTo: .callout)
+    /// Barlow, 100, 15
+    static let subheadline = CustomTextStyle(Barlow.thin, size: 15, relativeTo: .subheadline)
+    /// DancingScript, 700, 13
+    static let footnote = CustomTextStyle(DancingScript.bold, size: 13, relativeTo: .footnote)
+    /// DancingScript, 700, 12
+    static let caption = CustomTextStyle(DancingScript.bold, size: 12, relativeTo: .caption)
+    /// DancingScript, 700, 11
+    static let caption2 = CustomTextStyle(DancingScript.bold, size: 11, relativeTo: .caption2)
 }
 
-// Add bolder fonts for legibilityWeight in addition to those used.
-// Don't add cases for italic fonts, use the .italic() view modifier instead.
 fileprivate enum Barlow: String, CaseIterable, FontFamily {
     static let baseName = "Barlow-"
     
-    case thin, extraLight, regular, medium, semiBold, bold
+    case thin, extraLight, regular, medium
 }
 
 fileprivate enum DancingScript: String, CaseIterable, FontFamily {
     static let baseName = "DancingScript-"
     
-    case regular, medium, semiBold
+    case bold
 }
 
 fileprivate enum NewYorkExtraLarge: String, CaseIterable, FontFamily {
@@ -51,25 +50,15 @@ fileprivate enum NewYorkExtraLarge: String, CaseIterable, FontFamily {
     case semiBold, bold
 }
 
-fileprivate enum Copperplate: String, CaseIterable, FontFamily {
-    static let baseName = "Copperplate"
-    
-    case light = "-Light", regular = ""
-}
-
 struct FontView: View {
-    @Environment(\.legibilityWeight) private var legibilityWeight
+    
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
                 VStack(alignment: .leading) {
                     Text("Hello World!")
-                        .font(.custom(.hugeTitle))
-                    Text("Hello World!")
                         .font(.custom(.largeTitle))
-                    Text("Hello World!")
-                        .font(.custom(.largeTitle2))
                     Text("Hello World!")
                         .font(.custom(.title))
                     Text("Hello World!")
@@ -80,8 +69,6 @@ struct FontView: View {
                         .font(.custom(.headline))
                     Text("Hello World!")
                         .font(.custom(.body))
-                    Text("Hello World!")
-                        .font(.custom(.body2))
                 }
                 VStack(alignment: .leading) {
                     Text("Hello World!")
@@ -92,28 +79,8 @@ struct FontView: View {
                         .font(.custom(.footnote))
                     Text("Hello World!")
                         .font(.custom(.caption))
-                    Text("Hell.custom(o World!")
+                    Text("Hello World!")
                         .font(.custom(.caption2))
-                    Text("Hello World!")
-                        .font(.custom(.caption3))
-                }
-                VStack(alignment: .leading) {
-                    Text("Hello World!")
-                        .font(.custom(.hugeTitle))
-                    Text("Hello World!")
-                        .font(.custom(.hugeTitleFixed))
-                    Text("Hello World!")
-                        .font(.custom(.title3))
-                        .bold()
-                        .italic()
-                    Text("Hello World!")
-                        .font(.custom(.hugeTitle))
-                        .italic()
-                    Text("Hello ")
-                        .font(.custom(.hugeTitle), consider: legibilityWeight)
-                        .italic() +
-                    Text("World!")
-                        .font(.custom(.caption), consider: legibilityWeight)
                 }
             }
         }
